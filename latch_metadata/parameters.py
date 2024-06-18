eafdb80290f42a1197b7858d91969f3bdb9f5fbe
Tahir D'Mello <tahirdme@gmail.com>
2024-06-17 19:31:58 -0700

    Automated commit: processed repository radseq

---
+++ latch_metadata/parameters.py
@@ -0,0 +1,54 @@
+
+from dataclasses import dataclass
+import typing
+import typing_extensions
+
+from flytekit.core.annotation import FlyteAnnotation
+
+from latch.types.metadata import NextflowParameter
+from latch.types.file import LatchFile
+from latch.types.directory import LatchDir, LatchOutputDir
+
+# Import these into your `__init__.py` file:
+#
+# from .parameters import generated_parameters
+
+generated_parameters = {
+    'input': NextflowParameter(
+        type=LatchFile,
+        default=None,
+        section_title='Input/output options',
+        description='Path to comma-separated file containing information about the samples in the experiment.',
+    ),
+    'outdir': NextflowParameter(
+        type=typing.Optional[typing_extensions.Annotated[LatchDir, FlyteAnnotation({'output': True})]],
+        default=None,
+        section_title=None,
+        description='Path to the output directory where the results will be saved.',
+    ),
+    'email': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title=None,
+        description='Email address for completion summary.',
+    ),
+    'multiqc_title': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title=None,
+        description='MultiQC report title. Printed as page header, used for filename if not otherwise specified.',
+    ),
+    'genome': NextflowParameter(
+        type=typing.Optional[str],
+        default='null',
+        section_title='Reference genome options',
+        description='Name of reference genome',
+    ),
+    'need_to_trim_fastq': NextflowParameter(
+        type=typing.Optional[bool],
+        default=None,
+        section_title='Overide defaults',
+        description='enable trimming of reads prior to denovo clustering',
+    ),
+}
+
